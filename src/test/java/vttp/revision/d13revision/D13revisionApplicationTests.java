ac1c5e599202c019b42a2646588c4d302f3616dd
Gi Eon <95740792+koreanboiboi@users.noreply.github.com>
2022-07-21 09:48:37 +0800

    revision

---
+++ src/test/java/vttp/revision/d13revision/D13revisionApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp.revision.d13revision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D13revisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
